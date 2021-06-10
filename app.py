1caf962401aa7a476eca8577743e4f921fbf5c67
Rokuflame <KRS@d.c>
2021-06-10 21:02:49 +0300

    Update app.py
    начал разроботку взаимодействий

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import webbrowser
+import json
 import pyttsx3
+from random import choice
 import speech_recognition as sr
 
 engine = pyttsx3.init()     # создание объекта
@@ -21,10 +24,82 @@ engine.setProperty('voice', 'ru')
 for voice in voices:
     if voice.name == 'Anna':
         engine.setProperty('voice', voice.id)   # Попробовать установить предпочтительный голос
-engine.say('Привет мир: ' )
+
+def play_greetings(args: list):
+    greetings = ["Привет", "Приветик", "Приветствую", "Здраствуй господин", "что здесь забыл!"]
+    engine.say(choice(greetings))
+
+def play_farewell_and_quit(args: list):
+    farewell_and_quit = ["Пока", "Свидимся", "Жду тебя повелитель", "Пойду позалипаю", "Ну и проваливай"]
+    engine.say(choice(farewell_and_quit))
+
+def search_for_video_on_youtube(args: list):
+    search = " ".join(args)
+    webbrowser.get().open(f"https://www.youtube.com/results?search_query={search}")
+
+def search_for_definition_on_wikipedia(args: list):
+    pass
+
+
+def execute_command_with_name(command_name: str, *args: list):
+    """
+    Выполнение заданной пользователем команды и аргументами
+    :param command_name: название команды
+    :param args: аргументы, которые будут переданы в метод
+    :return:
+    """
+    for key in commands.keys():
+        if command_name in key:
+            commands[key](*args)
+        else:
+            pass    # print("Command not found")
+
+# перечень команд для использования (качестве ключей словаря используется hashable-тип tuple)
+# в качестве альтернативы можно использовать JSON-объект с намерениями и сценариями
+# (подобно тем, что применяют для чат-ботов)
+commands = {
+    ("hello", "hi", "morning", "привет", "приветики", "приветствую", "здраствуй"): play_greetings,
+    ("bye", "goodbye", "quit", "exit", "stop", "пока", "покеда", "прощай"): play_farewell_and_quit,
+    ("search", "google", "find", "найди"): "search_for_term_on_google",
+    ("video", "youtube", "watch", "видео", "ютьюб", "ютуб", "ютюб"): search_for_video_on_youtube,
+    ("wikipedia", "definition", "about", "определение", "википедия"): search_for_definition_on_wikipedia,
+    ("translate", "interpretation", "translation", "перевод", "перевести", "переведи"): "get_translation",
+    ("language", "язык"): "change_language",
+    ("weather", "forecast", "погода", "прогноз"): "get_weather_forecast",
+    ("facebook", "person", "run", "пробей", "контакт"): "run_person_through_social_nets_databases",
+    ("toss", "coin", "монета", "подбрось"): "toss_coin",
+}
+
+
+
+def record_volume():
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=1) as source:
+        print('Настраиваюсь.')
+        r.adjust_for_ambient_noise(source, duration=0.5)    # настройка посторонних шумов
+        print("Слушаю...")
+        audio = r.listen(source)
+    print("Услышала.")
+    try:
+        query = r.recognize_google(audio, language='ru-RU')
+        print(f'------------------------{query.lower()}----------------------')
+        voice_input = query.lower().split(' ')
+        command = voice_input[0]
+        command_options = [str(input_part) for input_part in voice_input[1:len(voice_input)]]
+        execute_command_with_name(command, command_options)
+    except:
+        print("Error")
+
 
 
 if __name__ == '__main__':
+    while True:
+        record_volume()
+        engine.runAndWait()
+        engine.stop()
 
-    engine.runAndWait()
-    engine.stop()
+
+# webbrowser.get(open("https://www.youtube.com/results?search_query=Коментанте")) команда для открытия страницы в браузере по умолчанию
+# https://www.youtube.com/results?search_query=Коментанте       | команда для поиска в ютубе
+# https://ru.wikipedia.org/w/index.php?search=          | команда для поиска на википедии
+#
